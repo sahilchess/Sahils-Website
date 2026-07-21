6330630ffc94064faec83e90fde5030d6ae4a898
Sahil Chess <125219792+sahilchess@users.noreply.github.com>
2026-07-21 18:03:08 +0000

    deployment

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'node:path'
 
 export default defineConfig({
-  base: '/Sahils-Website/docs/',
   plugins: [react()],
+  base: '/', 
   build: {
-  outDir: 'docs',
-  rollupOptions: {
-    input: {
-      main: 'index.html',
-      app: 'app.html'
-      }
-    }
+    outDir: 'dist',
   }
 })
